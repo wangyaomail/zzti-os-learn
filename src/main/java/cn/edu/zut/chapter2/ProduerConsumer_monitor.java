1077a68363569a68326785e29b801dce163ec2bb
wangyao <wangyao@helper12366.com>
2020-10-07 13:42:35 +0800

    fix count

--- src/main/java/cn/edu/zut/chapter2/ProduerConsumer_monitor.java
+++ src/main/java/cn/edu/zut/chapter2/ProduerConsumer_monitor.java
@@ -1,57 +1,57 @@
 package cn.edu.zut.chapter2;
-
 import java.util.ArrayList;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class ProduerConsumer_monitor {
-    LinkedBlockingQueue<Integer> items = new LinkedBlockingQueue<Integer>();
-    AtomicInteger num = new AtomicInteger(1);
-    static AtomicInteger count = new AtomicInteger(0);
-
-    public static void main(String[] args) throws Exception {
-        ProduerConsumer_monitor pc = new ProduerConsumer_monitor();
-        ArrayList<Thread> allThreads = new ArrayList<Thread>();
-        for (int i = 0; i < 10; i++) {
-            Thread p = pc.new Producer();
-            Thread c = pc.new Consumer();
+    LinkedBlockingQueue<Integer> items=new LinkedBlockingQueue<Integer>();
+    static int num=1, blank=0;
+    static AtomicInteger count=new AtomicInteger(0);
+    public static void main(String[] args) throws Exception{
+        ProduerConsumer_monitor pc=new ProduerConsumer_monitor();
+        ArrayList<Thread> allThreads=new ArrayList<Thread>();
+        for(int i=0;i<10;i++){
+            Thread p=pc.new Producer();
+            Thread c=pc.new Consumer();
             p.start();
             c.start();
             allThreads.add(p);
             allThreads.add(c);
         }
-        for (Thread t : allThreads) {
+        for(Thread t: allThreads){
             t.join();
         }
-        System.out.println(pc.items.size());
+        System.out.println("count="+count.get());
+        System.out.println("blank="+blank);
     }
-
     class Producer extends Thread {
-        public void run() {
-            int i = 0;
-            do {
-                try {
-                    items.put(num.getAndIncrement());
-                } catch (Exception e) {
+        public void run(){
+            int i=0;
+            do{
+                try{
+                    items.put(num++);
+                    count.incrementAndGet();
+                }catch(Exception e){
                     e.printStackTrace();
                 }
-            } while (++i < 100);
+            }while(++i<100);
         }
     }
-
     class Consumer extends Thread {
-        public void run() {
-            int i = 0;
-            do {
-                try {
-                    Integer pollNum = items.take();
-                    if (pollNum != null) {
-                        count.incrementAndGet();
+        public void run(){
+            int i=0;
+            do{
+                try{
+                    Integer pollNum=items.take();
+                    if(pollNum!=null){
+                        count.decrementAndGet();
+                    }else{
+                        blank++;
                     }
-                } catch (Exception e) {
+                }catch(Exception e){
                     e.printStackTrace();
                 }
-            } while (++i < 100);
+            }while(++i<100);
         }
     }
 }
